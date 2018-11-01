5a85f3ce2aedf9ab1e567640306c045e2856196a
René Ruiz <rne.eclipse@gmail.com>
2018-11-01 11:08:27 -0600

    :frog: Requisiciones show

---
+++ resources/views/Requisiciones/Barra/show.blade.php
@@ -0,0 +1,46 @@
+<nav class="navbar navbar-expand-lg navbar-light  sticky-top mb-2" style="background-color: #e3f2fd;">
+  <a class="navbar-brand" href={!! asset('/requisiciones?tipo='.$transaccion->tipo) !!}>
+    Requisición
+    <span class="badge border-info border text-info">
+      Información
+    </span>  
+  </a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavDropdown">
+    <ul class="navbar-nav mr-auto">
+      @if ($transaccion->tipo == 4)
+        <li class="nav-item">
+          <a class="nav-link" href="#"  onclick={!! "'eliminarPedido(".$transaccion->id.");'" !!}>Eliminar</a>
+        </li>
+      @endif
+    </ul>
+    @include('Dashboard.boton_salir')
+  </div>
+</nav>
+<input type="hidden" name="u" id="ubi" value="show">
+
+<script type="text/javascript">
+  function eliminarPedido(id){
+		return swal({
+			title: 'Eliminar registro',
+			text: '¿Está seguro? ¡El registro no podrá ser recuperado!',
+			type: 'warning',
+			showCancelButton: true,
+			confirmButtonColor: '#DD6B55',
+			confirmButtonText: 'Si, ¡Eliminar!',
+			cancelButtonText: 'No, ¡Cancelar!',
+			confirmButtonClass: 'btn btn-danger',
+			cancelButtonClass: 'btn btn-default',
+			buttonsStyling: false
+		}).then((result) => {
+			if (result.value) {
+				localStorage.setItem('msg','yes');
+				var dominio = window.location.host;
+				location.href = 'http://'+dominio+'/blissey/public/eliminarPedido/'+id+"/4";
+				$('#formulario').submit();
+			}
+		});
+	}
+</script>
--- resources/views/Requisiciones/show.blade.php
+++ resources/views/Requisiciones/show.blade.php
@@ -1,103 +1,99 @@
 @php
   setlocale(LC_ALL,'es');
 @endphp
-@extends('dashboard')
+@extends('principal')
 @section('layout')
-  <div class="col-md-12 col-sm-12 col-xs-12">
-    <div class="x_panel">
-      <div class="x_title">
-          @if($transaccion->tipo==4)
-            <h2>Requisición<small>Pendiente</small></h2>
-          @endif
-          @if($transaccion->tipo==5 || $transaccion->tipo==6)
-            <h2>Requisición<small>Atendida</small></h2>
-          @endif
-        <div class="clearfix"></div>
-        <div class="x_content">
-          <div class="row">
-            Opciones
-          </div>
-          <div class="" role="tabpanel" data-example-id="togglable-tabs">
-            <ul id="myTab" class="nav nav-tabs bar_tabs" role="tablist">
-              <li role="presentation" class="active">
-                <a href="#tab_content1" id="datos-tab" role="tab" data-toggle="tab" aria-expanded="true">Datos</a>
-              </li>
-              <li role="presentation" class="">
-                <a href="#tab_content2" id="otros-tab2" role="tab" data-toggle="tab" aria-expanded="false">Detalle</a>
-              </li>
-            </ul>
-            <div id="myTabContent" class="tab-content">
-              {{-- Datos --}}
-              <div role="tabpanel" class="tab-pane fade active in" id="tab_content1" aria-labelledby="datos-tab">
-                <table class="table">
-                  <tr>
-                    <th>Fecha</th>
-                    <td>{{$transaccion->fecha->formatLocalized('%d de %B de %Y')}}</td>
-                  </tr>
-                  <tr>
-                    <th>Fecha de creación</th>
-                    <td>{{ $transaccion->created_at->formatLocalized('%d de %B de %Y a las %H:%M:%S') }}</td>
-                  </tr>
-                  <tr>
-                    <th>Fecha de modificación</th>
-                    <td>{{ $transaccion->updated_at->formatLocalized('%d de %B de %Y a las %H:%M:%S') }}</td>
-                  </tr>
-                </table>
-              </div>
-              {{-- Detalles --}}
-              <div class="tab-pane fade" role="tabpanel" id="tab_content2" aria-labelledby="otros-tab2">
-                <table class="table table-striped">
-                  <thead>
-                    <th>Cantidad</th>
-                    <th colspan='2'>Detalle</th>
-                    @if ($transaccion->tipo==5 || $transaccion->tipo==6)
-                      <th>Lote</th>
-                    @endif
-                    @if ($transaccion->tipo==6)
-                      <th>Estante</th>
-                      <th>Nivel</th>
-                    @endif
-                  </thead>
-                  @php
-                  $detalles=$transaccion->detalleTransaccion;
-                  $total=0;
-                  @endphp
+	@include('Requisiciones.Barra.show')
+	<div class="col-sm-8">
+		<div class="x_panel">
+			<div class="flex-row">
+				<center>
+					<h5>Detalle</h5>
+				</center>
+			</div>
+			<table class="table table-striped table-sm table-hover">
+				<thead>
+					<th>Cantidad</th>
+					<th colspan='2'>Detalle</th>
+					@if ($transaccion->tipo==5 || $transaccion->tipo==6)
+						<th>Lote</th>
+					@endif
+					@if ($transaccion->tipo==6)
+						<th>Estante</th>
+						<th>Nivel</th>
+					@endif
+				</thead>
+				@php
+				$detalles=$transaccion->detalleTransaccion;
+				$total=0;
+				@endphp
+	
+				<tbody>
+				@foreach ($detalles as $detalle)
+					<tr>
+						<td>{{$detalle->cantidad}}</td>
+						<td>{{$detalle->divisionProducto->producto->nombre}}</td>
+						<td>
+							@if($detalle->divisionProducto->unidad==null)
+								{{$detalle->divisionProducto->division->nombre." ".$detalle->divisionProducto->cantidad." ".$detalle->divisionProducto->producto->presentacion->nombre}}
+							@else
+								{{$detalle->divisionProducto->division->nombre." ".$detalle->divisionProducto->cantidad." ".$detalle->divisionProducto->unidad->nombre}}
+							@endif
+						</td>
+						@if ($transaccion->tipo==5 || $transaccion->tipo==6)
+							<td>
+							{{$detalle->lote}}
+						</td>
+						@endif
+						@if ($transaccion->tipo==6)
+							<td>
+							{{$detalle->estante->codigo}}
+						</td>
+						<td>
+							{{$detalle->nivel}}
+						</td>
+						@endif
+					@endforeach
+				</tbody>
+	
+			</table>
+		</div>
+	</div>
+	<div class="col-sm-4">
+		<div class="x_panel">
+			<div class="flex-row">
+				<center>
+					<h4>Información General</h4>
+				</center>
+			</div>
 
-                  <tbody>
-                  @foreach ($detalles as $detalle)
-                    <tr>
-                      <td>{{$detalle->cantidad}}</td>
-                      <td>{{$detalle->divisionProducto->producto->nombre}}</td>
-                      <td>
-                        @if($detalle->divisionProducto->unidad==null)
-                          {{$detalle->divisionProducto->division->nombre." ".$detalle->divisionProducto->cantidad." ".$detalle->divisionProducto->producto->presentacion->nombre}}
-                        @else
-                          {{$detalle->divisionProducto->division->nombre." ".$detalle->divisionProducto->cantidad." ".$detalle->divisionProducto->unidad->nombre}}
-                        @endif
-                      </td>
-                      @if ($transaccion->tipo==5 || $transaccion->tipo==6)
-                        <td>
-                        {{$detalle->lote}}
-                      </td>
-                      @endif
-                      @if ($transaccion->tipo==6)
-                        <td>
-                        {{$detalle->estante->codigo}}
-                      </td>
-                      <td>
-                        {{$detalle->nivel}}
-                      </td>
-                      @endif
-                    @endforeach
-                  </tbody>
+			<div class="flex-row">
+				<span class="font-weight-light text-monospace">
+					Fecha
+				</span>
+			</div>
+			<div class="flex-row">
+				<h6 class="font-weight-bold">
+					{{$transaccion->fecha->formatLocalized('%d de %B de %Y')}}
+				</h6>
+			</div>
 
-                </table>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
+			<div class="ln_solid mb-1 mt-1"></div>
+			<div class="flex-row">
+				<span class="font-weight-light text-monospace">
+					Estado
+				</span>
+			</div>
+			<div class="flex-row">
+				<h6 class="font-weight-bold">
+					@if ($transaccion->tipo == 4)
+						<span class="badge border border-danger text-danger col-4">Pendiente</span>
+					@elseif($transaccion->tipo == 5 || $transaccion->tipo == 6)
+						<span class="badge border border-success text-success col-4">Atendida</span>
+					@endif
+				</h6>
+			</div>
+		</div>
+	</div>
 {!!Form::close()!!}
 @endsection
