72f91ce1b05367fc11889b13355eb2755f0698cc
Ingrid Ayala <ingridayala_94@hotmail.com>
2018-11-02 10:52:13 -0600

    Quitar report y pestaña para componenete

--- resources/views/Componentes/Barra/index.blade.php
+++ resources/views/Componentes/Barra/index.blade.php
@@ -19,9 +19,6 @@
       <li class="nav-item">
         <a class="nav-link" href={!! asset('/componentes/create') !!}>Nuevo</a>
       </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Reporte</a>
-      </li>
       <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
           Ver
