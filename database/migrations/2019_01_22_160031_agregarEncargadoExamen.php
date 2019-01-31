0665106647c77ba41403e86aff89ed634229f91f
HqzMer <opsa.al.el@gmail.com>
2019-01-30 18:11:32 -0600

    migration de encargado de evluar examen

---
+++ database/migrations/2019_01_22_160031_agregarEncargadoExamen.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AgregarEncargadoExamen extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('resultados', function (Blueprint $table) {
+          $table->integer('f_laboratorista')->unsigned()->nullable();
+          $table->foreign('f_laboratorista')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('resultados', function (Blueprint $table) {
+              $table->dropColumn('f_laboratorista');
+        });
+    }
+}
