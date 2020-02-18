1c063e162a2bb3e6299535ee2ca90030072dfea9
Ingrid Ayala <ingridayala_94@hotmail.com>
2020-02-18 15:58:14 -0600

    Corrección tamaño fuente exámenes

--- resources/views/SolicitudExamenes/entregaExamenQuimicaSanguinea.blade.php
+++ resources/views/SolicitudExamenes/entregaExamenQuimicaSanguinea.blade.php
@@ -1,5 +1,32 @@
-@extends('PDF.hoja')
-@section('layout')
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <!-- Meta, title, CSS, favicons, etc. -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Reporte</title>
+    <!-- Bootstrap -->
+    {!!Html::style('assets/bootstrap/dist/css/bootstrap.css')!!}
+    <!-- Font Awesome -->
+    {!!Html::style('assets/font-awesome/css/font-awesome.min.css')!!}
+
+
+    {!!Html::style('assets/build/css/custom.css')!!}
+    {!!Html::style('css/pdfexamenes.css')!!}
+
+    <style type="text/css">
+      div.page
+      {
+          page-break-after: always;
+          page-break-inside: avoid;
+      }
+    </style>
+  </head>
+
+  <body class="bg-white">
 @php
 	$fecha = Carbon\Carbon::now();
 	$banderaObservacion=false;
@@ -111,4 +138,5 @@
 			</div>
 		</div>
 	</div>
-@endsection
+</body>
+</html>
