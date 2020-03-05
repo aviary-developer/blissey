15d914dc3426bd69aa5763860593a1f3bce5c264
HqzMer <opsa.al.el@gmail.com>
2020-03-04 20:04:33 -0600

    Buscador de solicitudes entregadas

--- resources/views/SolicitudRayosx/Partes/lista_paciente_en.blade.php
+++ resources/views/SolicitudRayosx/Partes/lista_paciente_en.blade.php
@@ -1,7 +1,8 @@
+<input type="text" class="form-control form-control-sm" name="buscadorExamen" id="buscadorExamen" placeholder="Buscar paciente..." onkeyup="buscarEntregado(this);" autofocus>
 <div id="accordion">
   @foreach ($pacientes as $k => $paciente)
       
-    <div class="card">
+    <div class="card" id="cartas">
       <div class="card-header p-0" id={{"heading".$k}}>
         <h5 class="mb-0">
           <button class="btn btn-link" data-toggle="collapse" data-target={{"#collapse".$k}} aria-expanded="true" aria-controls={{"collapse".$k}}>
@@ -45,3 +46,17 @@
     </div>
   @endforeach
 </div>
+<script>
+  function buscarEntregado(texto){
+    console.clear();
+    $("#cartas div h5 button").each(function(){
+      let cadena=$(this).text().trim();
+      let posicion = cadena.toLowerCase().indexOf(texto.value.toLowerCase());
+      if (posicion !== -1){
+          $(this).parents("#cartas").attr( "style", "display: block !important;" );
+      }else{
+          $(this).parents("#cartas").attr( "style", "display: none !important;" );
+    }
+    });
+  }
+</script>
