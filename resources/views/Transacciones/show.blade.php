e299a956f44c0c97280ac1046d1a685e33caf98f
Ingrid Ayala <ingridayala_94@hotmail.com>
2020-04-13 15:58:34 -0600

    Corrección en tab list

--- resources/views/Transacciones/show.blade.php
+++ resources/views/Transacciones/show.blade.php
@@ -11,18 +11,18 @@
     <div class="x_panel">
         <ul class="nav nav-tabs" id="myTab" role="tablist">
           <li class="nav-item">
-            <a class="nav-link active active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">
+            <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">
               Detalle
             </a>
           </li>
           <li class="nav-item">
-            <a class="nav-link active" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">
+            <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">
               Devoluciones
             </a>
           </li>
           @if($transaccion->tipo!=2)
           <li class="nav-item">
-            <a class="nav-link active" id="profile-tab" data-toggle="tab" href="#next" role="tab" aria-controls="profile" aria-selected="false">
+            <a class="nav-link" id="profile-tab" data-toggle="tab" href="#next" role="tab" aria-controls="profile" aria-selected="false">
               Salidas
             </a>
           </li>
